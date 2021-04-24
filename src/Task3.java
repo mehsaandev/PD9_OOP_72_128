08452743533b3bffc9574b0e0c892bb3117cc088
ehsaan2611 <ehsaan2611@gmail.com>
2021-04-25 03:22:03 +0500

    Task 4 Done

--- src/Task3.java
+++ src/Task3.java
@@ -10,11 +10,12 @@
  */
 public class Task3 {
 
-    /** This method remove the multiple blank spaces from the String
-    * 
-    * @param s unremoved extra blank spaces string
-    * @return removed extra blank spaces string
-    */
+    /**
+     * This method remove the multiple blank spaces from the String
+     *
+     * @param s unremoved extra blank spaces string
+     * @return removed extra blank spaces string
+     */
     public static String ridMultipleBlank(String s) {
         String convertedString = null;
         for (int i = 0; i < s.length(); i++) {
@@ -24,15 +25,19 @@ public class Task3 {
             } else {
 
                 if (!(s.charAt(i) == ' ' && s.charAt(i - 1) == ' ')) {
+                    if (!(s.charAt(i - 1) == '.' && s.charAt(i) == ' ')) {
+                        convertedString = convertedString + s.charAt(i);
+                    }
 
-                    convertedString = convertedString + s.charAt(i);
                 }
             }
         }
         return convertedString;
     }
+
     /**
      * This Method remove the integer numbers from the String
+     *
      * @param s unremoved integer number string
      * @return removed integer number string
      */
@@ -46,15 +51,18 @@ public class Task3 {
         }
         return convertedString;
     }
+
     /**
-     * This Method Encrypts the String depending upon the number of character in a string and the ASCII of the character
+     * This Method Encrypts the String depending upon the number of character in
+     * a string and the ASCII of the character
+     *
      * @param s string to be encrypted
      * @return encrypted string
      */
     public static String stringEncryption(String s) {
         String encryptedString = "";
         for (int i = 0; i < s.length(); i++) {
-            encryptedString = encryptedString + ((char)(s.charAt(i)+(7+(i-(i*2)))));
+            encryptedString = encryptedString + ((char) (s.charAt(i) + (7 + (i - (i * 2)))));
         }
         return encryptedString;
     }
